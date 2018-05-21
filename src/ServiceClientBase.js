ab9f23d09eea76024d0eee5c8cd522acc086d33d
ert <ertugrulgerceker@fiyuu.com.tr>
2018-05-21 20:56:55 +0300

    ..

--- src/ServiceClientBase.js
+++ src/ServiceClientBase.js
@@ -212,6 +212,214 @@ class ServiceClientBase {
 ServiceClientBase.prototype.log = null;
 ServiceClientBase.prototype.baseUrl = null;
 
+class ServiceClientBase2 {
+
+	post(endpoint, principalId, requestContent) {
+
+		return new Promise((resolve, reject) => {
+
+			const time = hrtime();
+
+			const url = `${this.baseUrl}${endpoint}`;
+			const urlInfo = parseUrl(
+				url
+			);
+
+			let lib;
+			switch (urlInfo.protocol) {
+
+				case "http:":
+				case "ipc:":
+					lib = http;
+					break;
+
+				case "https:":
+				case "ipcs:":
+					lib = https;
+					break;
+
+				default:
+					throw new Error();
+			}
+
+			let payload;
+			if (requestContent === undefined) {
+				// ok
+			}
+			else {
+				payload = Buffer.from(
+					JSON.stringify(
+						requestContent
+					),
+					"utf8"
+				);
+			}
+
+			const options = {
+				method: "POST",
+				path: urlInfo.path
+			};
+
+			switch (urlInfo.protocol) {
+				case "ipc:":
+					options.protocol = "http:";
+					options.socketPath = `/tmp/${urlInfo.hostname}.sock`;
+					break;
+
+				case "ipcs:":
+					options.protocol = "https:";
+					options.socketPath = `/tmp/${urlInfo.hostname}.sock`;
+					break;
+
+				case "http:":
+				case "https:":
+					options.protocol = urlInfo.protocol;
+					options.hostname = urlInfo.hostname;
+					options.port = urlInfo.port;
+					break;
+
+				default:
+					throw new Error();
+			}
+
+			if (payload === undefined) {
+				// ok
+			}
+			else {
+
+				options.headers = {
+					"Content-Type": "application/json; charset=utf-8",
+					"Content-Length": `${payload.length}`
+				};
+			}
+
+			const request = lib.request(options, response => {
+
+				const chunks = [];
+
+				response.on("data", chunk => {
+
+					chunks.push(
+						chunk
+					);
+				});
+
+				response.on("error", error => {
+
+					reject(
+						error
+					);
+				});
+
+				response.on("end", () => {
+
+					let responseContent;
+
+					if (0 < chunks.length) {
+
+						try {
+							responseContent = JSON.parse(
+								Buffer.concat(chunks).toString("utf8")
+							);
+						}
+						catch (error) {
+
+							reject(
+								error
+							);
+
+							return;
+						}
+					}
+
+					const statusCode = response.statusCode;
+
+					switch (statusCode) {
+
+						case 200: {
+
+							if (responseContent === undefined) {
+								reject(
+									new Error("service-client::empty-response")
+								);
+							}
+							else if (responseContent.code === "ok") {
+
+								const [s, ns] = hrtime(time);
+								const elapsed = ((s * 1e9 + ns) / 1e6).toFixed(2);
+
+								this.log.trace(
+									"%s %s",
+									url,
+									elapsed
+								);
+
+								resolve(
+									responseContent.data
+								);
+							}
+							else {
+
+								const error = new Error(responseContent.code);
+								error.data = responseContent.data;
+
+								reject(
+									error
+								);
+							}
+
+							break;
+						}
+
+						default:
+
+							reject(
+								new Error(`http-${statusCode}`)
+							);
+							break;
+					}
+				});
+			})
+
+			request.on("error", error => {
+				reject(error);
+			});
+
+			if (payload === undefined) {
+				request.end();
+			}
+			else {
+				request.end(
+					payload
+				);
+			}
+		});
+	}
+
+	static create(methods) {
+
+		class ServiceClient extends ServiceClientBase2 { }
+
+		for (const key in methods) {
+
+			ServiceClient.prototype[key] = function (principalId, request) {
+
+				return this.post(
+					methods[key],
+					principalId,
+					request
+				);
+			}
+		}
+
+		return ServiceClient;
+	}
+}
+
+ServiceClientBase2.prototype.log = null;
+ServiceClientBase2.prototype.baseUrl = null;
+
 module.exports = {
-	ServiceClientBase
+	ServiceClientBase,
+	ServiceClientBase2
 };
